6f5444f3f86166616d0c281331bb21dc5cbb8d4a
meriDev <meridev9@gmail.com>
2025-03-17 18:43:40 +0000

    added user routes and controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+//@desc GET ALL PRODUCTS
+//@route GET /api/products
+//@access public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+
+  res.status(200).json(products);
+});
+
+//@desc GET SINGLE PRODUCT
+//@route GET /api/products/:id
+//@access public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.status(200).json(product);
+  } else {
+    res.status(404);
+    throw new Error('"Product not found');
+  }
+});
+
+export { getProducts, getProductById };
---
+++ backend/controllers/userController.js
@@ -0,0 +1,78 @@
+import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
+
+import User from '../models/userModel.js';
+
+//@desc AUTH USER AND GET TOKEN
+//@route POST /api/users/login
+//@access public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await User.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid Email or password');
+  }
+});
+
+//@desc CREATE USER
+//@route GET /api/users
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User Already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid User Data');
+  }
+});
+
+//@route GET /api/users/profile
+//@access private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('User Not Found');
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
