091efded008fa73f7cb77b126859e136eebedf3d
meriDev <meridev9@gmail.com>
2025-03-26 23:31:31 +0000

    -madded add order route in the backend

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config();
 
@@ -35,6 +36,7 @@ app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
