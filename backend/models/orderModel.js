f6f75d1dbba8722cb41f01b2f18fef586c80dfa0
meriDev <meridev9@gmail.com>
2025-03-27 01:27:32 +0000

    -madded add order in the api order  reducer

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -5,12 +5,12 @@ const orderSchema = mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'user',
+      ref: 'User',
     },
     orderItems: [
       {
         name: { type: String, required: true },
-        quantity: { type: Number, required: true },
+        qty: { type: Number, required: true },
         image: { type: String, required: true },
         price: { type: Number, required: true },
         product: {
@@ -24,13 +24,13 @@ const orderSchema = mongoose.Schema(
       address: { type: String, required: true },
       city: { type: String, required: true },
       postalCode: { type: String, required: true },
-      Country: { type: String, required: true },
+      country: { type: String, required: true },
     },
-    payementMethod: {
+    paymentMethod: {
       type: String,
       required: true,
     },
-    payementResult: {
+    paymentResult: {
       id: { type: String },
       status: { type: String },
       update_time: { type: String },
